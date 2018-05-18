4fddabf73ab9663fd0751f93417ff04e141ed6f4
Artem <artemciy@gmail.com>
2018-05-18 21:32:39 +0300

    Picked the primary color. Short description of the survey on the welcome screen.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  fillerNav = Array(50).fill(0).map((_, i) => `Nav ${i + 1}`);
-  fillerContent = Array(50).fill(0).map(() =>
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut');
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
