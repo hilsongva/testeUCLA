83dab05abb5b5e898c9ab0a69a461f5b8157e9c0
Artem <artemciy@gmail.com>
2018-05-20 22:13:24 +0300

    Get the question number from the router.

--- src/app/r-ucla/question.co.ts
+++ src/app/r-ucla/question.co.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'question-co',
   templateUrl: './question.co.html'
 })
-export class QuestionComponent {
-}
+export class QuestionComponent implements OnInit {
+  /** The question to display, 1-based. */
+  n?: number;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe ((params: ParamMap) => {
+      this.n = +params.get ('n')})}}
