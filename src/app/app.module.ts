595d8a6356d79d5dfbb0b22d788e74a658264060
Artem <artemciy@gmail.com>
2018-05-20 21:24:21 +0300

    Router introduced.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.co';
+import { WelcomeComponent } from './welcome.co';
+import { QuestionComponent } from './r-ucla/question.co'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'r-ucla/:n', component: QuestionComponent }];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, WelcomeComponent, QuestionComponent],
   imports: [
+    RouterModule.forRoot(ROUTES),
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    MatButtonModule
-  ],
+    MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/r-ucla/question.co.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'question-co',
+  templateUrl: './question.co.html'
+})
+export class QuestionComponent {
+}
