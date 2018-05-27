4cf2d508796c6b9fea5d33e2d093038203e2e351
Artem <artemciy@gmail.com>
2018-05-27 03:00:02 +0300

    Save the answers.

--- src/app/r-ucla/question.co.ts
+++ src/app/r-ucla/question.co.ts
@@ -3,7 +3,6 @@ import { MatDialog } from '@angular/material';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { ErrorComponent } from '../error.co';
 
-// TODO: Persist the answers in the browser.
 // TODO: Redirect back to the first question if we can't find the data.
 
 type Question = string
@@ -30,17 +29,24 @@ const QUESTIONS: ([Question, ReverseScored]|null)[] = [null,
   ['There are people I can talk to', true],  // 19.
   ['There are people I can turn to', true]]  // 20.
 
+type QuestionNumber = number;
+type NormalizedScore = number;
+interface QuestionScores {[question: string]: NormalizedScore}  // JSON keys are strings.
+interface Store {answers: QuestionScores}
+
+type TimeMs = number;
+
 @Component({
   templateUrl: 'question.co.html',
   styleUrls: ['question.co.scss']})
 export class QuestionComponent implements OnInit {
   /** The question to display, 1-based. */
-  num?: number;
+  num?: QuestionNumber;
   question?: Question;
   /** Time we started transitioning from question to question, fading out the old question. */
-  fadingOut: number | null = null;
+  fadingOut: TimeMs | null = null;
   /** Time we started transitioning from question to question, fading in the new question. */
-  fadingIn: number | null = null;
+  fadingIn: TimeMs | null = null;
 
   constructor(
     private router: Router,
@@ -59,13 +65,19 @@ export class QuestionComponent implements OnInit {
       } else {
         setTimeout (() => this.dialog.open (ErrorComponent, {disableClose: true, data: {msg: 'No such question'}}), 1)}})}
 
-  private proceed (num: number | undefined, rate: number) {
+  private proceed (num: QuestionNumber | undefined, score: NormalizedScore) {
     if (num == null) throw new Error ("!num");
     if (this.fadingOut || (this.fadingIn && Date.now() - this.fadingIn < 200)) return;  // Too late, we're already in transition.
     const tuple = QUESTIONS[num];
     const reverseScored = tuple ? tuple[1] : false;
-    const normalized = reverseScored ? 5 - rate : rate;
-    console.info ('num:', num, '; rate:', rate, '; reverseScored:', reverseScored, '; normalized:', normalized);
+    const normalized = reverseScored ? 5 - score : score;
+
+    // Save the answers, allowing the user to continue the questionnaire across the browser restarts.
+    const json = localStorage.getItem ('r-ucla');
+    let store: Store = json ? JSON.parse (json) : {answers: {}};
+    store.answers[num] = normalized;
+    localStorage.setItem ('r-ucla', JSON.stringify (store));
+
     if (QUESTIONS[num + 1]) {  // If there is a next question then route there.
       this.fadingOut = Date.now();
       setTimeout (() => {
